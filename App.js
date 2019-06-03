0ce7f663c3a60c3766a7333085f0d4d42d3102c0
EduardoHobold <38993334+EduardoHobold@users.noreply.github.com>
2019-06-02 21:27:39 -0300

    Calculadora de Indice de Massa Corporal

---
+++ App.js
@@ -0,0 +1,99 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, {Component} from 'react';
+import {StyleSheet, Text, View, TextInput, TouchableOpacity} from 'react-native';
+
+
+
+export default class App extends React.Component {
+
+  constructor(props){
+    super(props)
+    this.state = {altura:0, massa:0, resultado:0, resultadoText:""}
+    this.calcular = this.calcular.bind(this)
+  }
+
+  calcular(){
+
+    let imc = this.state.massa / (this.state.altura * this.state.altura)
+
+    let s = this.state
+    s.resultado = imc
+
+    if(s.resultado < 16){
+      s.resultadoText = "Mageza Grave"
+    }else if(s.resultado < 17){
+      s.resultadoText = "Magreza Moderada"
+    }else if(s.resultado < 18.5){
+      s.resultadoText = "Magreza Leve"
+    }else if(s.resultado < 25){
+      s.resultadoText = "Saudável"
+    }else if(s.resultado < 30){
+      s.resultadoText = "Sobrepeso"
+    }else if(s.resultado < 35){
+      s.resultadoText = "Obesidade Grau I"
+    }else if(s.resultado < 40){
+      s.resultadoText = "Obesidade Grau II"
+    }else{
+      s.resultadoText = "Obesidade Grau III"
+    }
+
+    this.setState(s)
+
+  }
+
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={styles.entradas}>
+            <TextInput placeholder="Massa" keyboardType="numeric" style={styles.input} onChangeText={(massa)=>{this.setState({massa})}}/>
+            <TextInput placeholder="Altura" keyboardType="numeric" style={styles.input} onChangeText={(altura)=>{this.setState({altura})}}/>
+        </View>
+        <TouchableOpacity style={styles.button} onPress={this.calcular}><Text style={styles.buttonText}>Calcular</Text></TouchableOpacity>
+        <Text style={styles.resultado}>{this.state.resultado.toFixed(2)}</Text>
+        <Text style={[styles.resultado,{fontSize:40}]}>{this.state.resultadoText}</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#F5FCFF',
+  },
+  input:{
+    height:80,
+    width:"50%",
+    fontSize:50,
+    marginTop:24,
+  },
+  button:{
+    backgroundColor: "#00b0ff"
+  },
+  entradas:{
+    flexDirection: 'row'
+  },
+  buttonText:{
+    alignSelf: 'center',
+    padding: 30,
+    fontSize: 25,
+    color:"#ffffff",
+    fontWeight: 'bold',
+  },
+  resultado:{
+    alignSelf: 'center',
+    color:"black",
+    fontSize:46,
+    padding:15
+  }
+
+
+});
